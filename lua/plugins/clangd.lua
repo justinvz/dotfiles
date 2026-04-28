03c877fd9264e77cd9797379496109f0d63dc87d
justin <justin.vanzuidam@rhmarine.com>
2026-04-28 20:54:05 +0200

    changes

---
+++ lua/plugins/clangd.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          -- cmd = { "/usr/bin/clangd-16" },
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/cmake.lua
@@ -0,0 +1,7 @@
+return {
+  "Civitasv/cmake-tools.nvim",
+  opts = {
+    cmake_configure_on_open = false,
+    cmake_regenerate_on_save = false,
+  },
+}
