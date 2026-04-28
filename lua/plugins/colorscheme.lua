4f46a5463066a6fd183cbdfd631253852650227c
justinvz <justin.vz@live.nl>
2026-04-28 21:03:06 +0200

    New theme!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,32 @@
 return {
   {
-    "Tsuzat/NeoSolarized.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
     config = function()
-      vim.cmd([[ colorscheme NeoSolarized ]])
+      require("rose-pine").setup({
+        disable_background = true,
+
+        styles = {
+          transparency = true,
+        },
+      })
+
+      vim.cmd("colorscheme rose-pine")
+
+      -- make ALL windows transparent
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+      vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+      vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+      vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
     end,
   },
 }
