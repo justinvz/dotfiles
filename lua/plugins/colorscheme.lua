8134b9c19a5a7041ec05b325c0c65b112f83a815
justin <justin.vanzuidam@rhmarine.com>
2026-04-29 21:43:24 +0200

    Added session manager and some other stuff!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,27 +6,12 @@ return {
     config = function()
       require("rose-pine").setup({
         disable_background = true,
-
         styles = {
           transparency = true,
         },
       })
 
       vim.cmd("colorscheme rose-pine")
-
-      -- make ALL windows transparent
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
-      vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
-      vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
-      vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-      vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
-      vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
     end,
   },
 }
