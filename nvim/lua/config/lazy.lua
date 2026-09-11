a0cf28c7328a606b85d05b7d8aede3ee67f80ce3
justinvz <justin.vanzuidam@rhmarine.com>
2026-09-11 11:00:32 +0200

    Changed lazy loading

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,9 +32,9 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+    enabled = false, -- run :Lazy check explicitly instead of once per Neovim instance
+    notify = false,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
--- nvim/lua/plugins/clangd.lua
+++ nvim/lua/plugins/clangd.lua
@@ -1,12 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      server = {
-        clangd = {
-          -- cmd = { "clang-16"}
-        },
-      },
-    },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.clangd = opts.servers.clangd or {}
+
+      local cmd = opts.servers.clangd.cmd or { "clangd" }
+      if not vim.tbl_contains(cmd, "--log=error") then
+        table.insert(cmd, "--log=error")
+      end
+      opts.servers.clangd.cmd = cmd
+    end,
   },
 }
